b2a2263a456a846100ac53ef0d9ab531f69d5430
HP <HP@LAPTOP-PI2VK6U3>
2025-09-03 15:23:17 +0530

    new commit

---
+++ Devops_5F8/src/Lbrce_Devops/HelloWorld.java
@@ -0,0 +1,62 @@
+  package Lbrce_Devops;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+uy
